aef847149f8ed6d256e6583a56a3993f215b0e6a
Dom <dmodica2@pride.hofstra.edu>
2015-10-09 20:51:03 -0400

    thanks to google got the last hw problem

--- Csharp/csharp_hw4.cs
+++ Csharp/csharp_hw4.cs
@@ -1,13 +1,15 @@
 using System;
 using System.Collections;
 
-public class A2
+
+
+public class A2 
 {
   protected virtual void f() {Console.WriteLine("A2.f");}
   protected virtual void g() {Console.WriteLine("A2.g");}
   protected virtual void h1() {Console.WriteLine("A2.h1");}
 }
-public class B2
+public class B2 
 {
   protected virtual void f() {Console.WriteLine("B2.f");}
   protected virtual void g() {Console.WriteLine("B2.g");}
@@ -16,16 +18,36 @@ public class B2
 
 
 
-public class C2
-{
-	public void f(){
-		A2 a2 = new A2();
-		a2.f();
-	}
-	public void g(){
-	
-	}
-	public void h1(){}
-	public void h2(){}
 
+
+
+public class C3:B2 
+{
+	public void call_g(){ this.g();} // this is where the trick lies, had to do some googling to finally get a hint
+	public void call_h2(){this.h2();} // so B2's methods are restricted to descendants of that class
+	//that doesn't mean we cannot define a new public method that refers to these protected methods....that is where the loop whole lies
+}
+
+public class C2 : A2
+{
+	public void call_f(){ this.f();} // have c2 inherit directly from A2
+	public void call_h1(){this.h1();}
+	public void call_h2(){ C3 c3 = new C3(); // create new references to a C3 object (which inherits from B2) and has public methods that return the protected functions
+		c3.call_h2();} // call the "hidden functions"
+	public void call_g(){C3 c3= new C3();
+		c3.call_g();}
+
+}
+
+public class hello{
+	public static void Main()
+	{
+		Console.WriteLine("Starting");
+		C2 c2 = new C2();
+		c2.call_f();
+		c2.call_g();
+		c2.call_h1();
+		c2.call_h2();
+
+	}
 }
