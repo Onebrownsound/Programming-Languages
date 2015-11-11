3bda10525e9484cd0835304a8792ba0c4fe66399
onebrownsound <dmodica2@pride.hofstra.edu>
2015-11-11 14:30:08 -0500

    Finished problem 2
    Problem 2 complete in somewhat pseudocode

--- fsharp/hw.fs
+++ fsharp/hw.fs
@@ -1,4 +1,4 @@
-//problem 1
+//problem warmup
 type direction = North | South | East | West;;
 let figure_out_direction (a_direction : direction)=
     match a_direction with
@@ -11,12 +11,24 @@ printfn "%f" ,figure_out_direction South;; // prints 1
 printfn "%f" ,figure_out_direction East;; // prints 2 
 printfn "%f" , figure_out_direction West;; // prints 3
 
-//problem 2 
+//problem 1 
 let gcd (a :int) (b: int) =
 	match a with
 	| 0 -> b
 	| _ -> gcd (b%a) a
 
+//problem 2
+// f is type float, i is for integers, fraction is for rational
+let add_reals = function
+	match x,y with
+	| f , i -> x + float(y)
+	| i , f -> float(x) + y
+	| f , fraction -> x + float(y.numerator/y.denominator)
+	| fraction , f -> float(x.numerator/x.denominator) + y
+	| fraction, i -> x.add(real(y,1))
+	| i, fraction -> y.add(real(x,1))
+	| fraction, fraction -> x.add(y)
+	| _ , _ -> x+y
 
 //problem 3
 rlist
